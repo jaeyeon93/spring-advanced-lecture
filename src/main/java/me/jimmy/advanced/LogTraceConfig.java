430d44d5587b457b8a3ab4d610a1d1e0b9a6d0fa
JaeyeonKIM <foodsksms@gmail.com>
2022-04-17 13:56:17 +0900

    Feature LogTraceConfig Edit

--- src/main/java/me/jimmy/advanced/LogTraceConfig.java
+++ src/main/java/me/jimmy/advanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package me.jimmy.advanced;
 
 import me.jimmy.advanced.trace.logtrace.FieldLogTrace;
 import me.jimmy.advanced.trace.logtrace.LogTrace;
+import me.jimmy.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+//        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
